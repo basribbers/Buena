5b8a537d925f467f3387e941e2acc86d19896dd1
basribbers <basribbers@gmail.com>
2021-08-15 12:06:55 +0200

    inschrijven

--- jcode/verstuurMailaf.js
+++ jcode/verstuurMailaf.js
@@ -9,14 +9,12 @@ let achternaam = document.getElementById('achternaam').value;
 let voornaam = document.getElementById('voornaam').value;
 let onderwerp = "afspraak van "+ voornaam+" "+achternaam;
     //checken verplichte invoervelden
-    if (achternaam!="" && voornaam!=""){
-    
+   
     let telefoon =   document.getElementById('telefoon').value;
     let email =   document.getElementById('email').value;
     let locatie =   document.getElementById('locatie').value;
-  
     let opmerking = document.getElementById('opmerking').value;
-
+    if (achternaam!="" && voornaam!="" && email!=""){
     let bericht = voornaam +" "+ achternaam+" "+telefoon+" "+email+" "+locatie+" "+opmerking;
     window.location.href =  "afspraak.php?onderwerp=inschrijfformulier "+voornaam +" "+ achternaam+"&bericht=" + bericht ;
     }
--- jcode/verstuurMailin.js
+++ jcode/verstuurMailin.js
@@ -1,16 +1,12 @@
 let submit = document.getElementById('submit');
-
-
 submit.onclick=functie;
 function functie()
-
 {
 let achternaam = document.getElementById('achternaam').value;
 let voornaam = document.getElementById('voornaam').value;
 let onderwerp = "inschrijfformulier van "+ voornaam+" "+achternaam;
     //checken verplichte invoervelden
-    if (achternaam!="" && voornaam!=""){
-    let straat =     document.getElementById('straat').value;
+        let straat =     document.getElementById('straat').value;
     let huisnummer =   document.getElementById('huisnr').value;
     let woonplaats=     document.getElementById('woonplaats').value;
     let postcode=     document.getElementById('postcode').value;
@@ -19,8 +15,8 @@ let onderwerp = "inschrijfformulier van "+ voornaam+" "+achternaam;
     let locatie =   document.getElementById('locatie').value;
     let type = document.getElementById('type').value;
     let opmerking = document.getElementById('opmerking').value;
+    if (achternaam!="" && voornaam!="" && email!="" && telefoon!="" && straat!="" && huisnummer!="" && woonplaats!=""  ){
     let bericht = `${voornaam} ${achternaam} ${straat} ${huisnummer} ${postcode} ${woonplaats} ${telefoon} ${email} ${locatie} ${type} ${opmerking}`;
-    //let bericht = voornaam +" "+ achternaam+" "+ straat +" "+huisnummer+" "+postcode+" "+woonplaats+" "+telefoon+" "+email+" "+locatie+" "+type+" "+opmerking;
     window.location.href =  "inschrijven.php?onderwerp=inschrijfformulier "+voornaam +" "+ achternaam+"&bericht=" + bericht ;
     }
     else{let melding = document.getElementById('melding');
