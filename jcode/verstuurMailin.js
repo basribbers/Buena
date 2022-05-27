d8b33b238e8fcd7078483a0d5a242aca9dea28ba
basribbers <basribbers@gmail.com>
2022-05-27 21:15:26 +0200

    foto's verwijderd

--- jcode/verstuurMailin.js
+++ jcode/verstuurMailin.js
@@ -15,10 +15,20 @@ let onderwerp = "inschrijfformulier van "+ voornaam+" "+achternaam;
     let locatie =   document.getElementById('locatie').value;
     let type = document.getElementById('type').value;
     let opmerking = document.getElementById('opmerking').value;
+	let akkoord = document.getElementById('akkoord').value;
     if (achternaam!="" && voornaam!="" && email!="" && telefoon!="" && straat!="" && huisnummer!="" && woonplaats!=""  ){
-    let bericht = `${voornaam} ${achternaam} ${straat} ${huisnummer} ${postcode} ${woonplaats} ${telefoon} ${email} ${locatie} ${type} ${opmerking}`;
+		if  (document.getElementById('akkoord').checked == true) {
+    let bericht = `${voornaam} ${achternaam} ${straat} ${huisnummer} ${postcode} ${woonplaats} ${telefoon} ${email} ${locatie} ${type} ${opmerking} ik ga akkoord met de voorwaarden`;
     window.location.href =  "inschrijven.php?onderwerp=inschrijfformulier "+voornaam +" "+ achternaam+"&bericht=" + bericht ;
+		}
+			else {
+				let melding = document.getElementById('melding');
+				melding.innerHTML = `Voor het inschrijven voor een cursus bij Buena Communicacon dient u akkoord te gaan met de algemene voorwaarden`;
+				melding.style.visibility="visible";
+			}
+			
     }
     else{let melding = document.getElementById('melding');
-        melding.removeAttribute("hidden");}
+	
+        melding.style.visibility="visible";}
 }
